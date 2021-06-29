de63365b64edf6dc4ee53bf31d53ee01d935cf9d
Ankit Mishra <34853863+ankitwayne@users.noreply.github.com>
2021-06-29 20:58:39 +0530

    Assignment Day 1

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+print("Enter your first number : ")
+x = int(input())
+print("Enter your second number")
+y = int(input())
+
+add = x + y
+sub = x - y
+mul = x * y
+div = x / y
+print("Addition is : ")
+print(add)
+print("Subtraction is : ")
+print(sub)
+print("Multiplication is : ")
+print(mul)
+print("Division is : ")
+print(div)
